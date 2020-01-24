f7b317fc40c907bea966d9b85ddda437905417f3
nedakinduryte <neda.kinduryte@gmail.com>
2020-01-24 22:32:00 +0000

    Simplified BarGraph and added real time data

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -5,56 +5,36 @@ import { Bar } from "@vx/shape";
 import { scaleLinear, scaleBand } from "@vx/scale";
 
 // We'll use some mock data from `@vx/mock-data` for this.
-const data = letterFrequency;
+//const data = letterFrequency;
 
 // Define the graph dimensions and margins
 const width = 500;
 const height = 500;
 const margin = { top: 20, bottom: 20, left: 20, right: 20 };
 
-// Then we'll create some bounds
-const xMax = width - margin.left - margin.right;
-const yMax = height - margin.top - margin.bottom;
-
 // We'll make some helpers to get at the data we want
 const x = (d: any) => d.letter;
 const y = (d: any) => +d.frequency * 100;
 
-// And then scale the graph by our data
-const xScale = scaleBand({
-    rangeRound: [0, xMax],
-    domain: data.map(x),
-    padding: 0.4
-});
-const yScale = scaleLinear({
-    rangeRound: [yMax, 0],
-    domain: [0, Math.max(...data.map(y))]
-});
-
-// Compose together the scale and accessor functions to get point functions
-const compose = (scale: any, accessor: any) => (data: any) =>
-    scale(accessor(data));
-const xPoint = compose(xScale, x);
-const yPoint = compose(yScale, y);
+interface ComponentProps {
+    data: { x: number; y: number }[];
+}
 
 // Finally we'll embed it all in an SVG
-export const BarGraph: React.SFC = () => {
+export const BarGraph: React.SFC<ComponentProps> = props => {
     return (
         <svg width={width} height={height}>
-            {data.map((d, i) => {
-                const barHeight = yMax - yPoint(d);
-                return (
-                    <Group key={`bar-${i}`}>
-                        <Bar
-                            x={xPoint(d)}
-                            y={yMax - barHeight}
-                            height={barHeight}
-                            width={xScale.bandwidth()}
-                            fill="#fc2e1c"
-                        />
-                    </Group>
-                );
-            })}
+            {props.data.map(point => (
+                <Group key={point.x}>
+                    <Bar
+                        x={point.x}
+                        y={point.y}
+                        height={500}
+                        width={10}
+                        fill="#fc2e1c"
+                    />
+                </Group>
+            ))}
         </svg>
     );
 };
