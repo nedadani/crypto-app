f035a09515b164c24848ac926d5f276d6ccc965b
nedakinduryte <neda.kinduryte@gmail.com>
2020-03-05 07:36:14 +0000

    Styled Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import logo from "../logo.png";
+import styled from "styled-components";
+
+const Nav = styled.header`
+    width: 100%;
+    height: 60px;
+`;
+
+const Logo = styled.img`
+    height: 40px;
+    padding: 10px 15px;
+`;
 
 export const Header: React.FC = () => {
     return (
         <>
-            <header>
-                <img src={logo} alt="Crypto App logo" />
-            </header>
+            <Nav>
+                <Logo src={logo} alt="Crypto App logo" />
+            </Nav>
         </>
     );
 };
