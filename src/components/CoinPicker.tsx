c36ad46351e0b7e45099fe9daa9afcdceacd4d97
nedakinduryte <neda.kinduryte@gmail.com>
2020-03-06 19:03:06 +0000

    Added Button Props

--- src/components/CoinPicker.tsx
+++ src/components/CoinPicker.tsx
@@ -1,15 +1,50 @@
 import React from "react";
+import styled from "styled-components";
 
 interface ButtonProps {
+    selected: boolean;
+}
+
+const Button = styled.button<ButtonProps>`
+    background-color: ${props => (props.selected ? "red" : "white")};
+`;
+
+interface ComponentProps {
     updateCoin(newCoin: string): void;
 }
 
-export const CoinPicker: React.FC<ButtonProps> = props => {
+export const CoinPicker: React.FC<ComponentProps> = props => {
+    const [active, setActive] = React.useState("BTC" as string);
+
     return (
         <>
-            <button onClick={() => props.updateCoin("BTC")}>Bitcoin</button>
-            <button onClick={() => props.updateCoin("ETH")}>Ether</button>
-            <button onClick={() => props.updateCoin("LTC")}>Litecoin</button>
+            <Button
+                selected={active === "BTC"}
+                onClick={() => {
+                    props.updateCoin("BTC");
+                    setActive("BTC");
+                }}
+            >
+                Bitcoin
+            </Button>
+            <Button
+                selected={active === "ETH"}
+                onClick={() => {
+                    props.updateCoin("ETH");
+                    setActive("ETH");
+                }}
+            >
+                Ether
+            </Button>
+            <Button
+                selected={active === "LTC"}
+                onClick={() => {
+                    props.updateCoin("LTC");
+                    setActive("LTC");
+                }}
+            >
+                Litecoin
+            </Button>
         </>
     );
 };
