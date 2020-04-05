538ba7cf07632749a3411f6e195bb7b54af366bc
nedakinduryte <neda.kinduryte@gmail.com>
2020-04-05 18:49:53 +0100

    Added App.styles file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Chart } from "./components/Chart/Chart.view";
 import { Header } from "./components/Header/Header.view";
-import "./App.css";
+import { Wrapper } from "./App.styles";
 
 const App: React.FC = () => {
     return (
-        <>
+        <Wrapper>
             <Header />
             <Chart />
-        </>
+        </Wrapper>
     );
 };
 
