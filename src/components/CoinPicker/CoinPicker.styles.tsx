72822a6cc4b403d31ef2e0077a052b3d9f9c005c
nedakinduryte <neda.kinduryte@gmail.com>
2020-04-11 15:17:19 +0100

    Reduced padding

--- src/components/CoinPicker/CoinPicker.styles.tsx
+++ src/components/CoinPicker/CoinPicker.styles.tsx
@@ -8,15 +8,15 @@ const Button = styled.button<ButtonProps>`
     height: 2em;
     width: 10em;
     border-radius: 15px;
-    background-color: ${props => (props.selected ? "#0a3d62" : "#fff")};
-    color: ${props => (props.selected ? "#fff" : "#2f3542")};
+    background-color: ${(props) => (props.selected ? "#0a3d62" : "#fff")};
+    color: ${(props) => (props.selected ? "#fff" : "#2f3542")};
     font-size: 0.8em;
     margin: 0 0.5em;
 `;
 
 const Wrapper = styled.div`
     text-align: center;
-    padding: 2.5em 0;
+    padding: 1em 0;
 `;
 
 export { Button, Wrapper };
