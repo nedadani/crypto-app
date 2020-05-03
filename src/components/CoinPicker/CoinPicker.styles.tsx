5ca87622c3bbe92db50ec0265c7d08c516666bb6
nedakinduryte <neda.kinduryte@gmail.com>
2020-05-03 13:37:50 +0100

    Updated coin picker styles

--- src/components/CoinPicker/CoinPicker.styles.tsx
+++ src/components/CoinPicker/CoinPicker.styles.tsx
@@ -8,8 +8,14 @@ const Button = styled.button<ButtonProps>`
     height: 2em;
     width: 10em;
     border-radius: 15px;
-    background-color: ${(props) => (props.selected ? "#0a3d62" : "#fff")};
-    color: ${(props) => (props.selected ? "#fff" : "#2f3542")};
+    background-color: ${(props) =>
+        props.selected
+            ? props.theme.secondary.buttons
+            : props.theme.primary.background};
+    color: ${(props) =>
+        props.selected
+            ? props.theme.primary.background
+            : props.theme.primary.text};
     font-size: 0.8em;
     margin: 0 0.5em;
 `;
