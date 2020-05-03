0c29c2c5fffc48fa19521d5877136e7e63413263
nedakinduryte <neda.kinduryte@gmail.com>
2020-05-03 13:57:44 +0100

    Updated the color of the date and time

--- src/components/DateTime/DateTime.styles.tsx
+++ src/components/DateTime/DateTime.styles.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const Wrapper = styled.div`
     margin: 0 0 0 auto;
     font-size: 0.8em;
+    color: ${(props) => props.theme.primary.text};
 `;
 
 export { Wrapper };
