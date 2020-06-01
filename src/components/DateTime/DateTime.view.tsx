10f893c9c4ea80f1feaaa090540a0ba554be55b6
nedakinduryte <neda.kinduryte@gmail.com>
2020-06-01 08:42:09 +0100

    Added seconds to date & time component

--- src/components/DateTime/DateTime.view.tsx
+++ src/components/DateTime/DateTime.view.tsx
@@ -1,34 +1,72 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Wrapper } from "./DateTime.styles";
 
+interface stateTypes {
+  weekDay: string;
+  day: number;
+  month: string;
+  year: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
 export const DateTime: React.FC = () => {
-    // Months are zero-indexed in js
-    const months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ];
+  const [time, setTime] = React.useState({
+    weekDay: "Sun",
+    day: 1,
+    month: "January",
+    year: 2020,
+    hours: 1,
+    minutes: 1,
+    seconds: 1,
+  } as stateTypes);
 
-    const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const today = new Date();
+      const weekDayIndex = today.getDay();
+      const monthIndex = today.getMonth();
 
-    const today = new Date();
+      setTime({
+        weekDay: weekDays[weekDayIndex],
+        day: today.getDate(),
+        month: months[monthIndex],
+        year: today.getFullYear(),
+        hours: today.getHours(),
+        minutes: today.getMinutes(),
+        seconds: today.getSeconds(),
+      });
+    }, 1000);
 
-    const monthIndex = today.getMonth();
-    const month = months[monthIndex];
+    return () => clearInterval(interval);
+  }, []);
 
-    const weekDayIndex = today.getDay();
-    const weekDay = weekDays[weekDayIndex];
+  // Months are zero-indexed in js
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-    const date = `${weekDay}, ${today.getDate()} ${month} ${today.getFullYear()} ${today.getHours()}:${today.getMinutes()}`;
+  const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-    return <Wrapper>{date}</Wrapper>;
+  const date = `${time.weekDay}, ${time.day} ${time.month} ${
+    time.year
+  } ${time.hours
+    .toString()
+    .padStart(2, "0")}:${time.minutes
+    .toString()
+    .padStart(2, "0")}:${time.seconds.toString().padStart(2, "0")}`;
+
+  return <Wrapper>{date}</Wrapper>;
 };
